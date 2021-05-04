731f19e3c2e294e2eb99defd0408b16ae61b61cd
qinzong <1003326090@qq.com>
2021-05-04 21:40:43 +0800

    web.xml主要配置了乱码的问题

--- spring-mvc-annotation/src/main/java/com/qin/controller/TestController.java
+++ spring-mvc-annotation/src/main/java/com/qin/controller/TestController.java
@@ -3,6 +3,7 @@ package com.qin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +22,11 @@ public class TestController {
         String s = "你好。注解开发的SpringMVC";
         int result = a+b;
         model.addAttribute("msg",s+" "+result);
+        return "test";
+    }
+    @PostMapping("/e/l")
+    public String luan(String name,Model model){
+        model.addAttribute("msg",name);
         return "test";
     }
 }
